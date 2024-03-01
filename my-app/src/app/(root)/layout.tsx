9b76a781c85e0a5f0e3d8439810f76e9c491dfc9
OnlineSourceS <97689711+OnlineSourceS@users.noreply.github.com>
2024-03-01 05:34:57 -0800

    .

--- my-app/src/app/(root)/layout.tsx
+++ my-app/src/app/(root)/layout.tsx
@@ -1,32 +1,17 @@
-import { cn } from "@/lib/utils"
- import { Inter as FontSans } from "next/font/google"
- import "@/app/globals.css"
-import { ReactNode } from "react"
-import { Navbar } from "@/components/component/navbar"
+import { cn } from "@/lib/utils";
+import { Inter as FontSans } from "next/font/google";
+import "@/app/globals.css";
+import { ReactNode } from "react";
+import { Navbar } from "@/components/component/navbar";
 
-  
-
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
-
-export default function RootLayout({ children }:{children: ReactNode} ) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-
-      >
-          <Navbar />
-        <main>
-           {children}
-          </main>
+      <body className={cn("min-h-screen bg-background font-sans antialiased")}>
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
-  )
+  );
 }
