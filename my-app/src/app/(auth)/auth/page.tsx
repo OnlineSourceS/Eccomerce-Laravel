3ac3d7760949fe14b34ed8c5cb74adc3b9fae4a7
OnlineSourceS <97689711+OnlineSourceS@users.noreply.github.com>
2024-02-29 22:22:21 -0800

    attached shadcn ui

---
+++ my-app/src/app/(auth)/auth/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function AuthPage() {
+  return (
+    <div>
+      <Button>Auth</Button>
+    </div>
+  )
+}
---
+++ my-app/src/app/(root)/layout.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils"
+ import { Inter as FontSans } from "next/font/google"
+ import "@/app/globals.css"
+
+  
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export default function RootLayout({ children } ) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      > {children}
+      </body>
+    </html>
+  )
+}
